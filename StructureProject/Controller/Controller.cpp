a598483adbbd8f5115606a097d959dbe6231fb69
Skylark <37351525+5kyl4rk@users.noreply.github.com>
2019-01-28 12:27:02 -0700

    Started project (yay)

---
+++ StructureProject/Controller/Controller.cpp
@@ -0,0 +1,19 @@
+//
+//  Controller.cpp
+//  StructureProject
+//
+//  Created by Fitzgerald, Skyler on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+Controller :: Controller()
+{
+    
+}
+
+void Controller :: start()
+{
+    cout << "Let's do something I guess ._." << endl;
+}
