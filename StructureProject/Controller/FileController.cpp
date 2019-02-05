cf44c82c965616a46ae5255847a2b2173ef0fd02
Skylark <37351525+5kyl4rk@users.noreply.github.com>
2019-02-05 13:17:18 -0700

    implemented method to read CrimeData

--- StructureProject/Controller/FileController.cpp
+++ StructureProject/Controller/FileController.cpp
@@ -7,3 +7,41 @@
 //
 
 #include "FileController.hpp"
+
+vector<CrimeData> FileController :: readCrimeDataToVector(string filename)
+{
+    std :: vector<CrimeData> crimeVector;
+    string currentCSVLine;
+    int rowCount = 0;
+    
+    ifstream dataFile(filename);
+    
+    //if the file exists at the path
+    if (dataFile.is_open())
+    {
+        //keep reading until you are at the end of the file
+        while (!dataFile.eof())
+        {
+            //grab each line from the CSV separated by the carriage return character
+            getline(dataFile, currentCSVLine, '\r');
+            //Exclude header row
+            if (rowCount != 0)
+            {
+                //Create a CrimeData instance from the line. Exculde a blank line (usually if opened seprately)
+                if(currentCSVLine.length() != 0)
+                {
+                    CrimeData row(currentCSVLine);
+                    crimeVector.push_back(row);
+                }
+            }
+            rowCount++;
+        }
+        dataFile.close();
+    }
+    else
+    {
+        cerr << "NO FILE" << endl;
+    }
+    
+    return crimeVector;
+}
