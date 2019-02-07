619e7aef1f47703ff28a0a22858f525c497ac3ed
Skylark <37351525+5kyl4rk@users.noreply.github.com>
2019-02-07 13:33:28 -0700

    started LinkedList.hpp

--- StructureProject/Model/Nodes/Linear/LinkedList.hpp
+++ StructureProject/Model/Nodes/Linear/LinkedList.hpp
@@ -11,4 +11,31 @@
 
 #include "List.hpp"
 
+using namespace std; //Used for keyword access. Use this to make sure your pointers are pointing to the right things
+
+template <class Type>
+class LinkedList : public List<Type>
+{
+protected:
+    LinearNode<Type> * front;
+    LinearNode<Type> * end;
+public:
+    //contructor
+    LinkedList();
+    //destructor
+    virtual ~LinkedList(); //Virtual so it can be overridden
+    //helper methods
+    int getSize() const;
+    LinearNode<Type> * getFront();
+    LinearNode<Type> * getEnd();
+    
+    //Structure methods
+    void add(Type item);
+    void addAtIndex(iint index, Type item);
+    Type getFromIndex(int index);
+    Type remove(int index);
+//    Type setAtIndex(int index, Type item);
+//    bool contains(Type item);
+};
+
 #endif /* LinkedList_h */
