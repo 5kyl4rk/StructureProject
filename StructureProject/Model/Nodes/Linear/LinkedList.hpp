a772bab23f273d78c9f32ae02a2390d5b936ae2a
Skylark <37351525+5kyl4rk@users.noreply.github.com>
2019-02-11 11:59:15 -0700

    finished linkedList

--- StructureProject/Model/Nodes/Linear/LinkedList.hpp
+++ StructureProject/Model/Nodes/Linear/LinkedList.hpp
@@ -87,7 +87,7 @@ void LinkedList<Type> :: addAtIndex(int index, Type item)
     }
     else
     {
-        LinearNode<Type> * toBeAdded = new LInearNode<Type>(item);
+        LinearNode<Type> * toBeAdded = new LinearNode<Type>(item);
         if(index == 0)
         {
             toBeAdded->setNextNode(front);
@@ -109,5 +109,83 @@ void LinkedList<Type> :: addAtIndex(int index, Type item)
     }
 }
 
+template <class Type>
+Type LinkedList<Type> :: getFromIndex(int index)
+{
+    assert(index >= 0 && index < this->size);
+    Type data;
+    
+    LinearNode<Type> * current = front;
+    
+    for (int position = 0; position < index; position++)
+    {
+        current = current->getNextNode();
+    }
+    
+    data = current->getData();
+    
+    return data;
+}
+
+template <class Type>
+Type LinkedList<Type> :: remove(int index)
+{
+    assert(index >= 0 && index < this->size);
+    
+    LinearNode<Type> * current = front;
+    LinearNode<Type> * toBeRemoved = nullptr;
+    LinearNode<Type> * previous = nullptr;
+    
+    Type removedData;
+    
+    if(index == 0)
+    {
+        toBeRemoved = front;
+        this->front = this->front->getNextNode();
+    }
+    else
+    {
+        for(int position = 0; position < index; position++)
+        {
+            previous = current;
+            current = current->getNextNode();
+        }
+        
+        toBeRemoved = current;
+        
+        if(index == this->size - 1)
+        {
+            previous->setNextNode(nullptr);
+            end = previous;
+        }
+        else
+        {
+            current = toBeRemoved->getNextNode();
+            previous->setNextNode(current);
+        }
+    }
+    this->size -= 1;
+    
+    removedData = toBeRemoved->getData();
+    delete toBeRemoved;
+    return removedData;
+}
+
+template<class Type>
+LinearNode<Type> * LinkedList<Type> :: getEnd()
+{
+    return this->end;
+}
+
+template<class Type>
+LinearNode<Type> * LinkedList<Type> :: getFront()
+{
+    return this->front;
+}
+
+int LinkedList<Type> :: getSize() const
+{
+    return this->size;
+}
 
 #endif /* LinkedList_h */
