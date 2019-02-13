492ef5232a2c307ad8ddbc231321d0b7a6a17c3e
Skylark <37351525+5kyl4rk@users.noreply.github.com>
2019-02-13 13:00:03 -0700

    added tester files

---
+++ StructureProject/Testers/LinearTester.cpp
@@ -0,0 +1,9 @@
+//
+//  LinearTester.cpp
+//  StructureProject
+//
+//  Created by Fitzgerald, Skyler on 2/13/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "LinearTester.hpp"
---
+++ StructureProject/Testers/LinearTester.hpp
@@ -0,0 +1,14 @@
+//
+//  LinearTester.hpp
+//  StructureProject
+//
+//  Created by Fitzgerald, Skyler on 2/13/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef LinearTester_hpp
+#define LinearTester_hpp
+
+#include <stdio.h>
+
+#endif /* LinearTester_hpp */
