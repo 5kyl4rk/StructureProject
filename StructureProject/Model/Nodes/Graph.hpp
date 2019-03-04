c8e5129cf40c8ca3e88422c5e85b5bac5e2c43f9
Skylark <37351525+5kyl4rk@users.noreply.github.com>
2019-03-04 13:38:37 -0700

    *cracks knuckles* started Graph

---
+++ StructureProject/Model/Nodes/Graph.hpp
@@ -0,0 +1,23 @@
+//
+//  Graph.hpp
+//  StructureProject
+//
+//  Created by Fitzgerald, Skyler on 3/4/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef Graph_h
+#define Graph_h
+
+#include <set>
+#include <queue>
+#include <assert.h>
+
+using namespace std;
+template <class Type>
+class Graph
+{
+    
+};
+
+#endif /* Graph_h */
