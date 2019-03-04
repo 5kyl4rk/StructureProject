674fb9c0ffa8ca0c1ddaca1af0eed7e6200a59fa
Skylark <37351525+5kyl4rk@users.noreply.github.com>
2019-03-04 13:48:03 -0700

    Graph: finished method declaration

--- StructureProject/Model/Nodes/Graph.hpp
+++ StructureProject/Model/Nodes/Graph.hpp
@@ -17,7 +17,42 @@ using namespace std;
 template <class Type>
 class Graph
 {
+private:
+    static const int MAXIMUM = 50;
+    bool adjacencyMatrix [MAXIMUM][MAXIMUM];
+    int weightCostMatrix [MAXIMUM][MAXIMUM];
+    Type graphData[MAXIMUM];
+    int vertexCount;
+    void depthFirtsTraversal(Graph<Type> & graph, int vertex, bool markedVertices[]);
+public:
+    Graph();
+    //Graph operations
     
+    void addVertex(const Type& value);
+    
+    //connect vertices
+    void addEdge(int source, int target);
+    void addEdgeUndirected(int source, int target);
+    void addEdgeCost(int source, int target, int cost);
+    //disconnect vertices
+    void removeEdge(int source, int target);
+    void removeEdgeUndirectec(int source, int target);
+    void removeEdgeCost(int source, int target);
+    
+    //accessors
+    Type& operator [] (int vertext);
+    Type operator [] (int vertex) const;
+    int size() const;
+    
+    //check connectionos
+    bool hasUndirectedConnection(int source, int target) const;
+    bool areConnected(int source, int target) const;
+    std:: set<int> neighbors(int vertex) const;
+    
+    //Traversals
+    void depthFirstTraversal(Graph<Type> & graph, int vertex);
+    void breadthFirstTraversal(Graph<Type> & graph, int vertex)
+    int costTraversal(Graph<Type> & graph, int vertex);
 };
 
 #endif /* Graph_h */
