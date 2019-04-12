8a6c93f2a66f851905f2ebcb208e2e010981e348
Skylark <37351525+5kyl4rk@users.noreply.github.com>
2019-04-12 13:13:38 -0600

    started BinaryTreeTester
    surprisingly, there wasn't any typos to fix this time

---
+++ StructureProject/Model/Nodes/Tree.hpp
@@ -0,0 +1,13 @@
+//
+//  Tree.hpp
+//  StructureProject
+//
+//  Created by Fitzgerald, Skyler on 4/12/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef Tree_h
+#define Tree_h
+
+
+#endif /* Tree_h */
---
+++ StructureProject/Testers/BinaryTreeTester.hpp
@@ -0,0 +1,25 @@
+//
+//  BinaryTreeTester.hpp
+//  StructureProject
+//
+//  Created by Fitzgerald, Skyler on 4/12/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef BinaryTreeTester_hpp
+#define BinaryTreeTester_hpp
+
+#include <iostream>
+#include "../Model/Nodes/BinaryTreeNode.hpp"
+//#include "../Model/Nodes/BinarySearchTree.hpp"
+
+class BinaryTreeTester
+{
+private:
+    BinaryTreeNode<int> testNode;
+    //BinarySearchTree<int> testTree;
+public:
+    void treeStuff();
+};
+
+#endif /* BinaryTreeTester_hpp */
